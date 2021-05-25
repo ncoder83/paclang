3ed2a07684185bd4533d5cfab042cf385d5f391e
Pascal Anglade <pascal.nglade@gmail.com>
2021-05-25 00:29:19 -0400

    revert changes to fit the current version of C#

--- Pac/Pac/CodeAnalysis/Binding/Binder.cs
+++ Pac/Pac/CodeAnalysis/Binding/Binder.cs
@@ -84,9 +84,8 @@ namespace PacLang.Binding
             var upperBound = BindExpression(syntax.UpperBound, TypeSymbol.Int);
 
             _scope = new BoundScope(_scope);
-
-            var isReadOnly = true;
-            var variable = BindVariable(syntax.Identifier, isReadOnly, TypeSymbol.Int);
+            
+            var variable = BindVariable(syntax.Identifier, isReadOnly: true, TypeSymbol.Int);
             var body = BindStatement(syntax.Body);
 
             _scope = _scope.Parent;
