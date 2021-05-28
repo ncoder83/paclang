e6c949024ef8e48b79a031a63e167dadba60bac7
Pascal Anglade <pascal.nglade@gmail.com>
2021-05-27 22:31:19 -0400

    add more check

--- PacRunner/Program.cs
+++ PacRunner/Program.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     ///  X - Add string concatenation
-    ///  Add highlighting for strings
+    ///  X - Add highlighting for strings
     ///  Add support for calling built-in functions
     ///  Add support for looking up functions
     ///  Add support for random
